8bcb77ee4e2c0cde614db2b9e27441c5254299fb
redyelruc <redyelruc1@hotmail.com>
2020-12-03 13:59:58 +0000

    Advent of Code - Day 3 - rename some variables

--- advent_of_code/Advent 3.py
+++ advent_of_code/Advent 3.py
@@ -6,13 +6,13 @@ lines = f.readlines()
 full_map = [line.replace('\n', '')*100 for line in lines]
 
 
-def count_trees(this_map, inc_right, inc_down):
+def count_trees(this_map, inc_x, inc_y):
     x, trees = 0, 0
     try:
-        for row in range(0, len(this_map), inc_down):
+        for row in range(0, len(this_map), inc_y):
             if this_map[row][x] == '#':
                 trees += 1
-            x = x + inc_right
+            x = x + inc_x
     except IndexError:
         return trees
     return trees
@@ -20,4 +20,4 @@ def count_trees(this_map, inc_right, inc_down):
 
 print(f'Task1: {count_trees(full_map, 3, 1)}')
 num_trees = [count_trees(full_map, x, y) for (x, y) in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]
-print(f'Task2: {reduce(lambda x, y: x*y, num_trees)}')
+print(f'Task2: {reduce(lambda x, y: x * y, num_trees)}')
